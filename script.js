12aeac4ed85578eddceef52d2fb1428ba3e014bb
Brice Whitefield <bricewhitefield@yahoo.com>
2021-03-21 22:14:17 -0500

    refractoring

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 let formEl = document.querySelector("#task-form"); 
 let tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-let createTaskHandler = function(event) { 
+let createTaskHandler = function (event) {
+  event.preventDefault();
+  let taskNameInput = document.querySelector("input[name='task-name']").value;
+  let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    event.preventDefault();
-    
-  let listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
-  }; 
+  
+  let listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
 
-formEl.addEventListener("click", createTaskHandler);
+  
+  let taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
 
 
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", createTaskHandler);
